fb431dd30fda9af4e4218f73f26ef471d64c8722
OsvaldoTorre <allone2911@gmail.com>
2023-06-15 05:28:55 -0600

    Creacion modelo productos

--- src/modelo/producto.java
+++ src/modelo/producto.java
@@ -9,5 +9,75 @@ package modelo;
  * @author OSVALDO
  */
 public class producto {
-    
+
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int categoriaId;
+    private boolean disponibilidad;
+
+    public producto(int id, String nombre, String descripcion, double precio, int categoriaId, boolean disponibilidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.categoriaId = categoriaId;
+        this.disponibilidad = disponibilidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCategoriaId() {
+        return categoriaId;
+    }
+
+    public void setCategoriaId(int categoriaId) {
+        this.categoriaId = categoriaId;
+    }
+
+    public boolean isDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio
+                + ", categoriaId=" + categoriaId + ", disponibilidad=" + disponibilidad + "]";
+    }
+
 }
