5025eeb6f7d6cccdd3b3e4b0792ba12ff206b2dc
OsvaldoTorre <allone2911@gmail.com>
2023-06-15 05:25:31 -0600

    Creacion modelo usuarios

--- src/modelo/usuario.java
+++ src/modelo/usuario.java
@@ -9,5 +9,42 @@ package modelo;
  * @author OSVALDO
  */
 public class usuario {
+    String user, email, password;
+
+    public usuario(String user, String email, String password) {
+        this.user = user;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "usuario{" + "user=" + user + ", email=" + email + ", password=" + password + '}';
+    }
+    
     
 }
